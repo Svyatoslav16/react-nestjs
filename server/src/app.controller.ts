2b424948586624b7edcf4573c04e783eb3ae2995
Svyatoslav Maryukha <svyatoslav.maryukha@lab.altkraft.com>
2021-05-30 19:41:14 +0300

    nestjs work

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -3,13 +3,10 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-    constructor(private readonly appService: AppService) {
-        // this.appService = appService;
-    }
+    constructor(private readonly appService: AppService) {}
 
     @Get()
     getHello(): string {
-        console.log('this: ', this);
         return this.appService.getHello();
     }
 }
