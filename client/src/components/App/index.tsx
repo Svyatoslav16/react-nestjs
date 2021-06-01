548adf37577ddec4109d1a15977f5d9dd6c69943
Svyatoslav Maryukha <svyatoslav.maryukha@lab.altkraft.com>
2021-06-01 21:14:31 +0300

    use CRA in client

--- client/src/components/App/index.tsx
+++
@@ -1,8 +0,0 @@
-import React, { useState } from 'react';
-
-const App: React.FunctionComponent = () => {
-    const [state, setState] = useState('Hello world1');
-    return <h1>{state}</h1>;
-};
-
-export default App;
